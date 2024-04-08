0ec973af22c51be3f0172da5a9bf2fb67bd40c91
KimHyoengJin <gudwls9966@naver.com>
2024-04-08 15:26:39 +0900

    make project

---
+++ kim_movie/src/test/java/com/example/kim_movie/KimMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kim_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
